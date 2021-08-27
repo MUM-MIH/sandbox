47c4d1735d6b7b8b215c28d8ff4421f9a80bac19
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-08-27 09:54:23 +0800

    Update hiddenoverview.js

--- hiddenoverview.js
+++ hiddenoverview.js
@@ -1,7 +1,7 @@
 //change table row based on sem-break //
   $("tr").each(function(){
     var col_val = $(this).find("td:eq(2)").text();
-    if (col_val == "swot vac"){
+    if( $.trim(col_val.toLowerCase() ) === "swot vac" )
       $(this).addClass('calemphasis');  //the selected class colors the row green//
     } else {
       $(this).addClass('bad');
