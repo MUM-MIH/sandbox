8183cfeb92229579233c4245ed9624e9c1ce3e61
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-08-27 09:47:28 +0800

    Update hiddenoverview.js

--- hiddenoverview.js
+++ hiddenoverview.js
@@ -1,3 +1,13 @@
 if (/section/.test(window.location.href)) {
         document.documentElement.className += ' has-script';   
 }
+//change table row based on sem-break //
+  $("tr").each(function(){
+    var col_val = $(this).find("td:eq(2)").text();
+    if (col_val == "swot vac"){
+      $(this).addClass('calemphasis');  //the selected class colors the row green//
+    } else {
+      $(this).addClass('bad');
+    }
+  });
+});
