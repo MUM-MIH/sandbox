d8d16ef9083d5f1b288488ddeea9a28eafca75f0
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-08-27 10:22:59 +0800

    Update hiddenoverview.js

--- hiddenoverview.js
+++ hiddenoverview.js
@@ -1,7 +1,7 @@
 $(function(){
   $("tr").each(function(){
     var col_val = $(this).find("td:eq(2)").text();
-    if( $.trim(col_val) === "swot" ){
+    if( $.trim(col_val) === "swot vac" ){
       $(this).addClass('selected');  //the selected class colors the row green//
     } else {
       $(this).addClass('bad');
