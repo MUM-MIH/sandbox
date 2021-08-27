ff4fb2a1daab479f8422e362a6d9337184b533c4
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-08-27 10:41:18 +0800

    Update addfeatures.js

--- addfeatures.js
+++ addfeatures.js
@@ -50,14 +50,15 @@ window.addEventListener('load',function(){
 		console.log('not Assessment Section or section 0')
 	}
 //change table row based on sem-break //
+
+$(function(){
   $("tr").each(function(){
     var col_val = $(this).find("td:eq(2)").text();
-    if (col_val == "swot vac"){
-      $(this).addClass('calemphasis');  //the selected class colors the row green//
+    if( $.trim(col_val.toLowerCase() ) === "swot vac" ){
+      $(this).addClass('selected');  //the selected class colors the row green//
     } else {
       $(this).addClass('bad');
     }
   });
-});
 //Close window.onload function	
 	});
