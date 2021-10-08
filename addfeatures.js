b83cf49149e91a874456f93789145ef3744a9878
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2021-10-08 08:44:06 +0800

    Update SOS info

--- addfeatures.js
+++ addfeatures.js
@@ -15,22 +15,22 @@ window.addEventListener('load',function(){
 	var SchoolSpecific=$('li.breadcrumb-item').text();
 	//make a variable of the section name
 	var assessmentSectionName=$('h3.sectionname').text();
-	//check if it is Education Excellence
-	if (SchoolSpecific.match(/Education Excellence|Faculty of Business and Economics/) != null && window.location.href.slice(-10) != "&section=0"){
+	//check if it is School of Science
+	if (SchoolSpecific.match(/School of Science/) != null && window.location.href.slice(-10) != "&section=0"){
 	//check if we are on the key contacts section
 	if (assessmentSectionName.match(/Key Contacts/) != null && window.location.href.slice(-10) != "&section=0"){
-		//check if the assessment text already exists
+		//check if the key contacts text already exists
 		if ($('.label:contains("Course Management Office")').length!=0){
 			console.log('text present')
 		}
 		else{
 			//input the text if it isn't already present, after the section name
 			console.log('text NOT present')
-			$('h3.sectionname:last').after('<div class="central-info"><h2>Course Management Office</h2><div class="contactscontent"><p><br />The School of Science Course Management Office (CMO) is your go-to hub for the administration of your enrolled course. In some cases, you may also want to approach your CMO for unit-specific issues. This could include instances when you want information on special consideration, unit discontinuation or intermission, or supplementary assessments.<br /><br />Tel: <strong>+ 603 5514 6186 / 6187 / 6120</strong><br />Email: <strong>scienceinquiries.my@monash.edu</strong><br /><br /><br /></p></div><hr /><h2>Library Resources and Contacts</h2><div class="contactscontent"><p><br />The Library and Learning Commons at Monash University Malaysia is not only a rich database of books. It also hosts learning resources, workshops and guides for undergraduate students. <a href="https://www.monash.edu.my/library" target="_blank">Search the Library.<br /></a></p><p>Some relevant resources and guides:</p><ul><li><a href="https://www.monash.edu.my/library/research/elearning-resources/research-learning-skills-video-recordings" target="_blank">Research and Learning Skills Videos</a></li><li><a href="https://www.monash.edu/rlo/research-writing-assignments/referencing-and-academic-integrity/academic-integrity/what-is-academic-integrity" target="_blank">Academic Integrity Tutorial</a></li><li><a href="https://www.monash.edu/rlo/research-writing-assignments/referencing-and-academic-integrity/academic-integrity/plagiarism-collusion-and-contract-cheating" target="_blank">Plagiarism Tutorial</a></li><li><a href="https://www.monash.edu/rlo/research-writing-assignments/assignment-types/oral-presentation" target="_blank">How to do an Oral Presentation</a></li><li><a href="https://www.monash.edu/rlo/study-skills" target="_blank">Study skills for university<br /><br /></a></li></ul><p>Tel: <strong>+603 5514 6199</strong><br />Email: <strong>mum-lib-contact@monash.edu</strong></p><p><a href="https://www.monash.edu.my/library/research/library-workshops-events" target="_blank">Library Workshops and Events Calendar</a><a href="https://www.monash.edu.my/library/research/library-workshops-events" target="_blank"> </a></p></div></div>')
+			$('h3.sectionname:last').after('<div class="central-info"> <h2>Education Management Office</h2> <div class="contactscontent"> <p> <br>The School of Science Education Management Office (EMO) is your go-to hub for the administration of your enrolled course. You can approach EMO for course advice, unit enrolment, and unit-specific matters (e.g. intermission, special consideration, and unit discontinuation). </p><span>Email:&nbsp;</span><a>scienceinquiries.my@monash.edu</a> <br>Live chat:&nbsp;<a href="https://studenthub.monash.edu.my/science/" target="_blank">https://studenthub.monash.edu.my/science/</a> <br>Tel: +603-55146186 /&nbsp; +603-55146187 /&nbsp;+603-55159743 / +603-5514 4990 / +603-5514 4402 <br><br><p></p></div><hr> <h2>Science Study Support</h2> <div class="contactscontent"> <p> <br><span>We offer these&nbsp;<a href="https://www.monash.edu.my/science/current/undergraduate/study-support" target="_blank">programs</a>&nbsp;to provide guidance and enhance&nbsp;your learning experience:</span> <br></p><div> <ol> <li>Mentor-mentee program <br></li><li>Peer Mentoring program <br></li><li>Peer Assisted Study Sessions (PASS) program <br></li></ol> </div><br></div><hr> <h2>Student Support Services</h2> <div class="contactscontent"> <p> <br>Our&nbsp; <a href="https://www.monash.edu.my/student-services" target="_blank">Student Services</a>&nbsp;cover a range of support services, including services for timetable management, study abroad, health and wellbeing, counselling services, disability services, insurance, scholarship and financial assistance, exam matter, student pass, international student attendance requirement, career services, graduation, document or student letter requests,&nbsp; </p><div> </div><br></div><hr> <h2>Library Resources and Contacts</h2> <div class="contactscontent"> <p> <br>The Library and Learning Commons hosts learning resources, workshops, and guides for undergraduate students. Here are some useful&nbsp;<a href="https://www.monash.edu.my/library/research" target="_blank">undergraduate workshops</a>&nbsp;and&nbsp; <a href="https://www.monash.edu.my/library/research/self-study-support" target="_blank">self-study support</a>: </p><ul> <li>Search Skills <br></li><li>Academic writing <br></li><li>Summarising, paraphrasing and synthesizing <br></li><li>Managing references&nbsp; <br></li><li>Academic integrity with citing&nbsp;and referencing &nbsp; <br></li></ul> <p>You can also reach out to our&nbsp;<a href="https://www.monash.edu.my/library/research/school-liaison" target="_blank">School of Science Liaison</a>&nbsp;for individual or group consultation, to seek advice on your assignments, and to improve your research and study skills.&nbsp;</p><p> <a href="https://www.monash.edu.my/library/research/library-workshops-events" target="_blank"></a> <a href="https://www.monash.edu.my/library/research/library-workshops-events" target="_blank"></a> </p></div></div>')
 		}
 	}
 	else{
-		console.log('not Assessment Section or section 0')
+		console.log('not Key Contacts Section or section 0')
 	}
 	}
 		
@@ -47,7 +47,7 @@ window.addEventListener('load',function(){
 		}
 	}
 	else{
-		console.log('not Assessment Section or section 0')
+		console.log('not Important Policy Section or section 0')
 	}
 //change table row based on sem-break //
   $("tr").each(function(){
