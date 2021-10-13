0b1d0ecd54cfeacbc7c747004cde9710434e998b
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-10-13 19:16:25 +0800

    Update and rename studiosity.js to studiositybanner.js

---
+++ studiositybanner.js
@@ -0,0 +1,14 @@
+window.addEventListener('load',function(){
+  //make a variable of the activity names
+	var Malaysia=$('span.instancename').text();
+	//Get URL of studiosity activity
+	var StudioLink=$('span.instancename').parent().attr('href');
+		if (Malaysia.match(/Studiosity/)){
+			$('aside.block-region').before('<a class="studiobanner" href="URL" target="_blank"><img src="https://mum-mih.github.io/sandbox/Monash-portrait.png" style="width:275px;margin-bottom:15px"></a></div>')
+		}
+	else{
+		console.log('Studiosity not present')
+	}
+	//add the URL to banner
+    $('a.studiobanner').attr("href",StudioLink);
+  	});
