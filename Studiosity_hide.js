d458ae12ec73b205a4d7b147bf893fdb8c59559c
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-11-23 22:29:52 +0800

    Create Studiosity_hide.js
    This code is a WIP to hide the studiosity activity.

---
+++ Studiosity_hide.js
@@ -0,0 +1,8 @@
+<script>  
+  window.addEventListener('load',function(){
+  var Malaysia=$('span.instancename').text();
+    if (Malaysia.match(/Studiosity/)){
+        document.getElementById("section-0").style.display = "none";
+    }
+    	});
+  </script>
