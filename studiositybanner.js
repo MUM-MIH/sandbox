ebb003f4678911358fc6c0371bfe3a83175f8c91
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-11-23 15:50:06 +0800

    Update studiositybanner.js

--- studiositybanner.js
+++ studiositybanner.js
@@ -4,7 +4,7 @@ window.addEventListener('load',function(){
 	//Get URL of studiosity activity
 	var StudioLink=$('span.instancename').parent().attr('href');
 		if (Malaysia.match(/Studiosity/)){
-			$('aside.block-region').before('<a class="studiobanner" href="URL" target="_blank"><img src="https://mum-mih.github.io/sandbox/Monash-portrait.png" style="width:275px;margin-bottom:15px"></a></div>')
+			$('aside.block-region').before('<a class="studiobanner" href="URL" target="_blank"><img src="https://mum-mih.github.io/sandbox/Studiosity_Monash_SquareFA.jpg" style="width:275px;margin-bottom:15px"></a></div>')
 		}
 	else{
 		console.log('Studiosity not present')
