576892687db5ca8062b0d07cdb6cda2bc52e0fcd
Carynlim <89447349+Carynlim@users.noreply.github.com>
2021-11-23 21:44:10 +0800

    Fixed: URL to studiosity activity only
    Previous code was linking to the first activity on the Moodle page not the Studiosity activity.

--- studiositybanner.js
+++ studiositybanner.js
@@ -1,8 +1,9 @@
+<script>
 window.addEventListener('load',function(){
   //make a variable of the activity names
 	var Malaysia=$('span.instancename').text();
 	//Get URL of studiosity activity
-	var StudioLink=$('span.instancename').parent().attr('href');
+  var StudioLink=$("span.instancename:contains('Studiosity')").parent().attr('href');
 		if (Malaysia.match(/Studiosity/)){
 			$('aside.block-region').before('<a class="studiobanner" href="URL" target="_blank"><img src="https://mum-mih.github.io/sandbox/Studiosity_Monash_SquareFA.jpg" style="width:275px;margin-bottom:15px"></a></div>')
 		}
@@ -12,3 +13,4 @@ window.addEventListener('load',function(){
 	//add the URL to banner
     $('a.studiobanner').attr("href",StudioLink);
   	});
+  </script>
