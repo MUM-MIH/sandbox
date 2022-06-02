c9e2651ad2642d86c4650f54fd8dee8192c75c4e
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2022-06-02 22:02:31 +0800

    Update welcomevid.js

--- welcomevid.js
+++ welcomevid.js
@@ -1,5 +1,7 @@
 // directs the function to work once the page has loaded.
 window.addEventListener('load',function(){
+var general = document.getElementById("id_generalhdr");
+   general.classList.add("collapsed");
 	 
 //make a variable of the breadcrumbs 
 	var Edit=$('li.breadcrumb-item').text();
