d3e00852791482c297233e198fa57addb24e873e
Lalitha1124 <106790969+Lalitha1124@users.noreply.github.com>
2022-06-03 12:19:09 +0800

    Update welcomevid.js

--- welcomevid.js
+++ welcomevid.js
@@ -19,29 +19,27 @@ window.addEventListener('load',function(){
 	function getURL() {
       var video = document.querySelectorAll(".embed-responsive-16by9")[0];
       var videoURL = document.getElementById("url").value;
-      console.log(videoURL);
-        video.innerHTML=videoURL;
+   
       
       var uSyntab = document.querySelectorAll(".acctab-content p")[0];
       var uSynNew = document.getElementById("USny").value;
-      console.log(uSyntab);
-        uSyntab.innerHTML=uSynNew;
+      
       
       var LOstab = document.querySelectorAll(".acctab-content ol")[0];
       var LOsNew = document.getElementById("id_lo").value;
-      console.log(LOstab);
-        LOstab.innerHTML=LOsNew;
+    
       
       var Nametab = document.querySelectorAll(".acctab-content h6")[0];
       var NameNew = document.getElementById("Lname").value;
-      console.log(Nametab);
-        Nametab.innerHTML=NameNew;
+      
       
       var Contab = document.querySelectorAll(".acctab-content p")[2];
       var ConNew = document.getElementById("LC").value;
-      console.log(Contab);
-        Contab.innerHTML=ConNew;
-		
+ 
+if( videoURL == ""|| uSynNew =="" || LOsNew ==""|| NameNew =="" || ConNew =="")
+      {
+      return false
+      } 	
 		$("#id_introeditoreditable").focus();
 	}
 	
