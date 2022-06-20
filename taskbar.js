d8978029f5ca0fd4b3682cd98646bd3dec9a6579
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2022-06-20 22:21:16 +0800

    Update taskbar.js

--- taskbar.js
+++ taskbar.js
@@ -10,7 +10,31 @@ window.addEventListener('load',function(){
 //check if edit settings page and if hidden overview element exists
     if (Edit.match(/Edit settings/)){      
       if(taskbar){
-        $('#id_generalhdr').before('<fieldset class="clearfix collapsible" id="taskpicker"> <legend class="ftoggler" id="yui_3_17_2_1_1655205039382_698"><a href="#" class="fheader" role="button" aria-controls="id_modstandardelshdr" aria-expanded="true">Common module settings</a></legend> <button id="addtask">Add task</button> <div id="taskoption"> <div id="taskdrop" class="form-group row fitem "> <div class="col-md-3 col-form-label d-flex pb-0 pr-md-0"> <label class="d-inline word-break " for="id_visible"> Icon </label> </div> <div class="col-md-9 form-inline align-items-start felement" data-fieldtype="modvisible"> <select class="custom-select" name="visible" id="id_visible"> <option value="1" selected="">Play video</option> <option value="2">Book</option> <option value="3">Speech bubble</option> <option value="4">Pre-class symbol</option> </select> </div> </div> <div id="taskname" class="form-group row fitem "> <div class="col-md-3 col-form-label d-flex pb-0 pr-md-0"> <label class="d-inline word-break " for="id_cmidnumber"> Task name </label> </div> <div class="col-md-9 form-inline align-items-start felement" data-fieldtype="text"> <textarea type="text" class="tasknamez" name="cmidnumber" id="ids"></textarea> </div> </div> </div> </fieldset>')
+        $('#id_generalhdr').before('// directs the function to work once the page has loaded.
+window.addEventListener('load',function(){
+	 
+//make a variable of the breadcrumbs 
+	var Edit=$('li.breadcrumb-item').text();
+  
+//check if hidden overview exists  
+  var taskbar = document.getElementById("taskbar");
+
+//check if edit settings page and if hidden overview element exists
+    if (Edit.match(/Edit settings/)){      
+      if(taskbar){
+        $('#id_generalhdr').before('// directs the function to work once the page has loaded.
+window.addEventListener('load',function(){
+	 
+//make a variable of the breadcrumbs 
+	var Edit=$('li.breadcrumb-item').text();
+  
+//check if hidden overview exists  
+  var taskbar = document.getElementById("taskbar");
+
+//check if edit settings page and if hidden overview element exists
+    if (Edit.match(/Edit settings/)){      
+      if(taskbar){
+        $('#id_generalhdr').before('<fieldset class="clearfix collapsible" id="taskpicker"> <legend class="ftoggler" id="yui_3_17_2_1_1655205039382_698"><a href="#" class="fheader" role="button" aria-controls="id_modstandardelshdr" aria-expanded="true">Common module settings</a></legend> <button id="addtask">Add task</button> <div id="taskcontainer"> <div id="taskoption"> <div id="taskdrop" class="form-group row fitem "> <div class="col-md-3 col-form-label d-flex pb-0 pr-md-0"> <label class="d-inline word-break " for="id_visible"> Icon </label> </div> <div class="col-md-9 form-inline align-items-start felement" data-fieldtype="modvisible"> <select class="custom-select" name="visible" id="id_visible"> <option value="1" selected="">Play video</option> <option value="2">Book</option> <option value="3">Speech bubble</option> <option value="4">Pre-class symbol</option> </select> </div> </div> </div> </fieldset> <br> <button id="change">Create taskbar</button>')
 				  }
     }
   var btn = document.getElementById("addtask");
@@ -30,13 +54,11 @@ function textBoxCreate() {
 
 var btnchange = document.getElementById("change");
 btnchange.addEventListener("click", createTable);
-var btnchange = document.getElementById("iconbtn");
-btnchange.addEventListener("click", changeicon);
 
 function createTable() {
 
   var num_rowz = 2;
-  var num_cols = document.querySelectorAll("#taskname .tasknamez").length;
+  var num_cols = document.querySelectorAll("#taskdrop").length;
   var theader = '<table id="newtable" border="1">\n';
   var tbody = "";
   for (var i = 0; i < num_rowz; i++) {
@@ -54,7 +76,7 @@ function createTable() {
 
 function changeicon() {
 
-  var num_cols = document.querySelectorAll("#taskname .tasknamez").length;
+  var num_cols = document.querySelectorAll("#taskdrop").length;
   var table = document.getElementById("newtable");
  	var selectedValue;
     for (var i = 0; i<num_cols; i++) { //iterate trough rows
@@ -81,3 +103,4 @@ function changeicon() {
 }
 	//Close window.onload function	
 	});
+			
