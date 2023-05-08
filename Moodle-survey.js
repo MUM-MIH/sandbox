10e5a710491a6fe1a58a1baa019f16e21eb84889
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2023-05-08 13:27:02 +0800

    Create Moodle-survey.js

---
+++ Moodle-survey.js
@@ -0,0 +1,2 @@
+ //test add igraduate survey ad
+    $("#region-main").before('<a href="https://igraduate.fra1.qualtrics.com/jfe/form/SV_dbxyDeXZBpF5hZA?ins=330111&rort=r&abc=1" target="_blank"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
