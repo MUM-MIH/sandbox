0808e1fd40487ed9d2dc0cf012ce139d823e7a87
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2024-02-05 21:58:30 +0800

    Update studiosity_test.js

--- studiosity_test.js
+++ studiosity_test.js
@@ -1,15 +1,14 @@
 // directs the function to work once the page has loaded.
 window.addEventListener("load", function () {
- 
-   //add essfs advertisement
-$("#region-main").before('<a href="https://essfs.ureview.org/" target="_blank"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle_2023S2.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
- 
- //add igraduate survey ad
-$('aside.block-region').before('<a href="https://igraduate.fra1.qualtrics.com/jfe/form/SV_dbxyDeXZBpF5hZA?ins=330111&rort=r&abc=1" target="_blank"><img src="https://mum-mih.github.io/sandbox/studentbaro.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:275px;margin-bottom:15px"></a>');
 
 //add studiosity link
-$(".assessment-summary-widget").after('<a href="https://essfs.ureview.org/" target="_blank"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle_2023S2.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
+$(".assessment-summary-widget").after('<a href="https://essfs.ureview.org/" style="text-decoration: none; color: inherit;" target="_blank"><div class="container-fluid border border-dark p-0 m-0"><div class="row gx-0 p-0"><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity1.png" alt="Image 1"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity2.png" alt="Image 2"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity3.png" alt="Image 3"></div></div></div></a>
+');
+
+ $("#coursecontentsection4").after('<a href="https://essfs.ureview.org/" style="text-decoration: none; color: inherit;" target="_blank"><div class="container-fluid border border-dark p-0 m-0"><div class="row gx-0 p-0"><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity1.png" alt="Image 1"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity2.png" alt="Image 2"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity3.png" alt="Image 3"></div></div></div></a>
+');
  
- 
+$("#section-19").after('<a href="https://essfs.ureview.org/" style="text-decoration: none; color: inherit;" target="_blank"><div class="container-fluid border border-dark p-0 m-0"><div class="row gx-0 p-0"><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity1.png" alt="Image 1"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity2.png" alt="Image 2"></div><div class="col-12 col-md-4 p-0"><img class="img-fluid" src="https://mum-mih.github.io/sandbox/studiosity3.png" alt="Image 3"></div></div></div></a>');
+
   //Close window.onload function
 });
