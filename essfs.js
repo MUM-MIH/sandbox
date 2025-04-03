5934459e8372fc03b7b30724aa96421e77908908
Lalitha1124 <106790969+Lalitha1124@users.noreply.github.com>
2025-04-03 12:58:26 +0800

    Update essfs.js

--- essfs.js
+++ essfs.js
@@ -1,6 +1,6 @@
-
+window.addEventListener("load", function () {
 
 //Add essfs advertisement banner , currently disabled
   $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/ESSFS-Banner-HN-edit.gif" alt="Image showing ipad and grab vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 300 grab vouchers and an Ipad air 128GB" style="width:100%;margin-bottom:15px"></a>');
 
-
+});
