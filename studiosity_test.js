07d8bb6227831c5da21af5a73a08ddf220c850e4
Lalitha1124 <106790969+Lalitha1124@users.noreply.github.com>
2025-04-03 12:08:00 +0800

    Update studiosity_test.js

--- studiosity_test.js
+++ studiosity_test.js
@@ -16,6 +16,6 @@ window.addEventListener("load", function () {
     $(".assessment-summary-widget").after(contentHtml);
 
 //Add essfs advertisement banner , currently disabled
-  $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/ESSFS_Banner_HN_edit.gif" alt="Image showing ipad and grab vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 500 grab vouchers and an Ipad air 64BG" style="width:100%;margin-bottom:15px"></a>');
+  $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/ESSFS-Banner-HN-edit.gif" alt="Image showing ipad and grab vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 500 grab vouchers and an Ipad air 64BG" style="width:100%;margin-bottom:15px"></a>');
 
 });
