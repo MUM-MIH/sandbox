86383bcfda0c154b0f17543ba8ca2b27a220e567
Lalitha1124 <106790969+Lalitha1124@users.noreply.github.com>
2026-03-26 14:07:49 +0800

    Update essfs.js

--- essfs.js
+++ essfs.js
@@ -1,8 +1,26 @@
 window.addEventListener("load", function () {
     const params = new URLSearchParams(window.location.search);
     const path = window.location.pathname;
-if (path.includes('course/view.php') || params.has('section')) {
+//if (path.includes('course/view.php') || params.has('section')) {
 //Add essfs advertisement banner, currently disabled
-  $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/MESS_Banner.png" alt="Image showing ipad and vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 300 grab vouchers and an Ipad air 128GB" style="width:100%;margin-bottom:15px"></a>');
-}
+//  $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/MESS_Banner.png" alt="Image showing ipad and vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 300 grab vouchers and an Ipad air 128GB" style="width:100%;margin-bottom:15px"></a>');
+//}
+//});
+
+        if (target) {
+            target.insertAdjacentHTML(
+                "beforebegin",
+                '<a href="https://mum.bluera.com/essfs/" target="_blank">' +
+                '<img src="https://mum-mih.github.io/sandbox/MESS_Banner.png" ' +
+                'alt="Image showing ipad and vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 300 grab vouchers and an Ipad air 128GB" ' +
+                'style="width:100%;margin-bottom:15px">' +
+                '</a>'
+            );
+            console.log("Banner added successfully");
+        } else {
+            console.log("#region-main not found");
+        }
+    } else {
+        console.log("Not a course page or no section parameter");
+    }
 });
