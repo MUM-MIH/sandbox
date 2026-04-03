31fe05966115593ff950f8069084ead6896604c5
Lalitha1124 <106790969+Lalitha1124@users.noreply.github.com>
2026-04-03 20:49:49 +0800

    Update essfs.js

--- essfs.js
+++ essfs.js
@@ -7,7 +7,7 @@ if (path.includes('course/view.php') || params.has('section')) {
   $("#region-main").before('<a href="https://mum.bluera.com/essfs/" target="_blank"><img src="https://mum-mih.github.io/sandbox/MESS_Banner.png" alt="Image showing ipad and vouchers with the text, Take the Early Semester Student Feedback Survey and stand a chance to win 300 grab vouchers and an Ipad air 128GB" style="width:100%;margin-bottom:15px"></a>');
 }
 //});
-
+/*
         if (target) {
             target.insertAdjacentHTML(
                 "beforebegin",
@@ -18,5 +18,5 @@ if (path.includes('course/view.php') || params.has('section')) {
                 '</a>'
             );
            
-        }
-});
+        } 
+}); */
